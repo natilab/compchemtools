d53dfd0de1e57206b2fb0335f746a4e806a9d216
nat <nati.labadie@gmail.com>
2021-07-06 22:06:43 -0300

    Adds files for g09job class, writing g09 files and whole hcs processing

---
+++ hcs_to_g09.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 13:12:08 2021
+
+@author: nat
+"""
+
+#hcs_to_g09.py
+
+"""Script for the processing of .HCS files into g09 input files. 
+Conformation geometries are extracted from HCS files.
+g09 job, method, route can be provided and g09 input files are created 
+with the geometries.
+Useful information from conformational search is extracted and written
+into csv file.
+"""
+
+#%%modules
+
+import os
+
+import proc_hcs
+from write_g09in import g09_job
+
+
+
+#%% process hcs file, output list of molecules 
+
+def get_mols(hcs_file, charge = 0, multiplicity = 1):
+    """Processes hcs_file into a dictionary of molecule object for 
+    each found conformation or conformer.
+    """
+    
+    return proc_hcs.main(hcs_file, charge, multiplicity)
+
+
+
+
+#%% generate g09_job
+
+def create_g09ins(mol_list, nproc, mem, func, basis, job, chk = None):
+    """mol_list: list of molecules as molecule objects.
+    nproc: int, number of processors.
+    mem: int, maximum memory (in GB).
+    func: string, functional to use.
+    basis: string, basis set to use.
+    job: string, g09 job keywords and options as would appear 
+    in input file.
+    chk: string, name of chk file, default None (no chk generated).
+    Out: list of g09_job objects, each one generated from a molecule 
+    of the input list.
+    """
+    
+    g09_jobs = []
+    
+    for molecule in mol_list:
+        current_job = g09_job(molecule, nproc, mem, func, 
+                 basis, job, chk)
+        g09_jobs.append(current_job)
+    
+    return g09_jobs
+
+    
+
+
+#%% write g09 inputs in subfolder
+
+def write_g09ins(g09_jobs, molname, extension, path):
+    """g09_jobs: list of g09_job objects.
+    molname: string, for main name of files.
+    extension: string, extension of file ('.com', '.gjf').
+    path: string, folder to work in. default: '.' (working directory).
+    Out: writes g09 input files in subfolder within provided path.
+    """
+    os.mkdir(os.path.join(path, 'g09_inputs'))
+    
+    for i, job in enumerate(g09_jobs):
+        filename = molname + str(i) + extension
+        job.write_input(os.path.join(path, 'g09_inputs', filename))
+
+
+#%% write .csv file with conformational search info
+
+def write_confSearch(mol_list, molname, path):
+    """Writes csv file in provided path with conformer energy and found
+    values for each one in the mol_list."""
+    
+    with open(os.path.join(path, molname+'.csv'), 'w') as f:
+        f.write('conformer, energy, found \n')
+        for i, molecule in enumerate(mol_list):
+            f.write(f'conf {i}, {molecule.energy}, {molecule.found} \n')
+    
+    
+
+#%% main function
+
+def main(hcs_file, charge = 0, multiplicity = 1, nproc = 4, mem = 2, 
+         func = 'B3LYP', basis = '6-31G*', job = '', chk = None, 
+         molname = 'mol', extension = '.com', path = '.'):
+    """Writes g09 inputs from hcs file and a csv file with
+    energy and found values for each conformer."""
+    
+    mol_list = get_mols(hcs_file, charge, multiplicity)
+    job_list = create_g09ins(mol_list, nproc, mem, func, basis, job, chk)
+    write_g09ins(job_list, molname, extension, path)
+    write_confSearch(mol_list, molname, path)
+
+
+#%% 
+
+if __name__ == '__main__':
+    import argparse as ap
+    parser = ap.ArgumentParser(prog = 'hcs_to_g09', 
+                               description = 'Write g09 input files from .HCS file')
+    
+    parser.add_argument('input', type = str, 
+                        help = 'input .HCS file')
+    parser.add_argument('-c', '--charge', type = int, default = 0)
+    parser.add_argument('-m', '--mult', type = int, default = 1,
+                        help = 'multiplicity')
+    parser.add_argument('-p', '--path', type = str, default = '.',
+                        help = 'path to folder for writing files')
+    parser.add_argument('-N', '--name', type = str, default = 'mol',
+                        help = 'molecule name for generated input files')
+    parser.add_argument('-o', '--out', type = str, default = '.com',
+                        help = 'file extension for g09 files')
+    parser.add_argument('-n', '--nproc', type = int, default = 4,
+                        help = 'Number of processors to use')
+    parser.add_argument('-M', '--mem', type = int, default = 4,
+                        help = 'Memory to use, in GB')
+    parser.add_argument('-f', '--func', type = str, default = 'B3LYP',
+                        help = 'functional to use, must be accepted by g09')
+    parser.add_argument('-b', '--basis', type = str, default = '6-31G*',
+                        help = 'basis set to use')
+    parser.add_argument('-j', '--job', type = str, default = '',
+                        help = f'''g09 job keywords and options as would appear in input file.
+                        Use single quotation marks around whole string when blankspaces are used.''')
+    parser.add_argument('-C', '--chk', type = str, default = None,
+                        help = 'name of chk file')
+    
+    args = parser.parse_args()
+    
+    main(hcs_file = args.input, charge = args.charge, multiplicity = args.mult,
+         nproc = args.nproc, mem = args.mem, func = args.func, basis = args.basis,
+         job = args.job, chk = args.chk, molname = args.name, extension = args.out,
+         path = args.path)
+    
+
+    
+
+    
+    
+    
+     
+
---
+++ write_g09in.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  5 14:24:48 2021
+
+@author: nat
+"""
+
+"""Set of functions for writing g09 input files from molecular geometry and
+job and route section details."""
+
+
+#%% modules
+
+from molecule import Coordinate, Molecule
+
+
+#%% define class for g09 input
+
+class g09_job(Molecule):
+    """Docstring
+    """
+    
+    
+    def __init__(self, molecule, nproc, mem, func, 
+                 basis, job, chk):
+        super(self).__init__(self, molecule.coordinates, molecule.atom_types)
+        self.charge = molecule.charge
+        self.mult = molecule.mult # multiplicity
+        self.nproc = nproc #number of processors used
+        self.mem = mem # maximum memory
+        self.chk = chk # chk name, if none, line is not written in out
+        self.func = func # functional
+        self.basis = basis # basis set
+        self.job = job # job keywords and options as string
+        self.comment = None
+        self.additional = False # for now the True option is not written
+    
+    def get_link0(self):
+        """Returns lines for link0 command section of g09 input as a list"""
+        
+        link0 = [f'%nprocshared={self.nproc}',
+                 f'%Mem={self.mem}GB']
+        
+        if self.chk:
+            link0.insert(0, f'%chk={self.chk}')
+        
+        return link0
+    
+    def get_route(self):  
+        """Return route section line for g09 input as string"""
+        
+        return f'# {self.func}/{self.basis} {self.job}'
+    
+    def get_specs(self):
+        """Returns string with line for initial molecule specifications 
+        (charge and multiplicity).
+        """
+        
+        return f'{self.charge}  {self.multiplicity}'
+    
+    def get_coords(self):
+        """Returns list of strings for cartesian coordinates needed in
+        g09 input. Each element would be a line in the input file."""
+        
+        return [f'{self.atom_types[atom]}  {self.coordinates[atom].x}  {self.coordinates[atom].y}  {self.coordinates[atom].z}' 
+                for atom in self.atom_types]
+    
+    def write_input(self, file_name):
+        """Writes input file into provided filename with g09 input format
+        using Cartesian Coordinates. Encoding: ASCII"""
+        
+        blank = '\n\n'
+        
+        with open(file_name, 'w', encoding = 'ascii') as out:
+            
+            # Write link0 command lines, turn list from getlin0 into lines
+            out.writelines("%s\n" % l for l in self.get_link0()) 
+            out.write(blank)
+            
+            # Write route section, as returned from get_route
+            out.write(self.get_route())
+            out.write(blank)
+            
+            # Write comment line
+            if self.comment:
+                out.write(self.comment)
+            else:
+                out.write('comment line')
+            out.write(blank)
+            
+            # Write Molecule Specifications
+            out.write(self.get_specs())
+            out.write('\n')
+            
+            #   atomic coordinates
+            out.writelines("%s\n" % l for l in self.get_coords())
+            
+            # Final blank lines
+            out.write('\n\n')
+    
+    
+    
+    
+        
+    
+
