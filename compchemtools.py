69076b368e317cc2e75d623c2c048833f99020d1
nat <nati.labadie@gmail.com>
2021-12-17 16:48:28 -0300

    Finished and tested wrapper

--- compchemtools.py
+++ compchemtools.py
@@ -24,13 +24,55 @@ def get_task():
           2: Write .sh files for g09 input files.
           3: Process conformational search results (.hcs).
           4: Change link0 and route lines in existing g09 input files.''')
-    task_num = int(input("Enter task number:"))
-    
+    task_num = input("Enter task number:")
+    if not task_num.strip().isdigit():
+        print("Error. Input must be an integer between 1 and 4.")
+        task_num = get_task()
+    else:
+        if int(task_num) > 4 or int(task_num) < 0:
+            print("Error. Input must be an integer between 1 and 4.")
+            task_num = get_task()
+        else:
+            task_num = int(task_num)
+            
     return task_num
 
 
 #%% functions to get additional arguments according to task
 
+def get_mem():
+    try:
+        mem = int(input("Memory to use (in GB, only write number):"))
+    except ValueError:
+        print("Error. Memory must be an integer.")
+        mem = get_mem()
+    return mem
+
+def get_nproc():
+    try:
+        nproc = int(input("Number of processors to use (nproc):"))
+       
+    except ValueError:
+        print("Error. nproc must be an integer.")
+        nproc = get_nproc()    
+    return nproc
+ 
+def get_charge():
+    try:
+        charge = int(input("Molecule charge:"))
+    except ValueError:
+        print("Error. Charge must be an integer.")
+        charge = get_charge()
+    return charge
+
+def get_mult():
+    try:
+        mult = int(input("Molecule multiplicity:"))
+    except ValueError:
+        print("Error. Multiplicity must be an integer.")
+        mult = get_mult()
+    return mult
+        
 def get_args_1():
     """Get arguments for task 1 from command line input."""
     
@@ -56,8 +98,7 @@ def get_args_2():
     path = input("Path to g09 input files:")
     extension = input("Extension of g09 output files (.com/.gjf):")
     time = input('Wall clock time for job, format HH:MM:SS:')
-    nfiles = int(input('Number of g09 inputs in each sh \
-                       (all must have same nproc):'))
+    nfiles = int(input('Number of g09 inputs in each sh (all must have same nproc):'))
     
     return path, extension, time, nfiles
 
@@ -75,18 +116,18 @@ def get_args_3():
     mps = []
     
     if mp == 'n':
-        charge = int(input("Molecule charge:"))
-        mult = int(input("Molecule multiplicity:"))
+        charge = get_charge()
+        mult = get_mult()
         mps = mps + [charge, mult]
     
     gj = input('Default g09 job input? (y/n):')
     gjs = []
     
     if gj == 'n':
-        nproc = int(input("Number of processors (nproc):"))
-        mem = int(input("Memory to use (in GB, only write number):"))
-        func = int(input("Functional:"))
-        bset = int(input("Basis set:"))
+        nproc = get_nproc()
+        mem = get_mem()
+        func = input("Functional:")
+        bset = input("Basis set:")
         job = input("G09 job keywords and options as would appear in input file:")
         gjs = gjs + [nproc, mem, func, bset, job]
     
@@ -94,14 +135,17 @@ def get_args_3():
     
 
 
+    
 def get_args_4():
     """Get arguments for task 4 from command line input."""
     
     # task 4: rewrite g09 files. Arguments needed for rw_g09in.py
     path = input("Path to g09 input files:")
     extension = input("Extension of g09 output files (.com/.gjf):")
-    nproc = int(input("Number of processors to use (nproc):"))
-    mem = int(input("Memory to use (in GB, only write number):"))
+    
+    nproc = get_nproc()
+    mem = get_mem()    
+        
     route = input("Route line for input (without # symbol):")
     chk = input("Add chk line to input files? (y/n):")
     
@@ -156,7 +200,7 @@ def task3():
         job = gjs[4]
     else:
         nproc = 4
-        mem = 1
+        mem = 2
         func = 'B3LYP'
         bset = '6-31G*'
         job = ''
@@ -178,3 +222,24 @@ def task4():
     rw_g09in.main(path, extension = extension, route = route, chk = chk, 
                   mem = mem, nproc = nproc)
 
+
+#%% main
+
+def main():
+    
+    task = get_task()
+    
+    if task == 1:
+        task1()
+    elif task == 2:
+        task2()
+    elif task == 3:
+        task3()
+    elif task == 4:
+        task4()
+
+    
+#%%
+
+if __name__ == '__main__':
+    main()
