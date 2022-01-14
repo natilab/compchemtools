26ef5c53646f5852127d7295c0b340e67192f8a3
nat <nati.labadie@gmail.com>
2022-01-14 18:51:47 -0300

    Added file to write cartesian coords for SI and rearranged file locations

---
+++ cctools/compchemtools.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 17:27:50 2021
+
+@author: nat
+"""
+
+# compchemtools.py
+
+"""Interactive wrapper for computational chemistry scripts."""
+
+#%% modules
+
+
+
+#%% function to get task
+
+def get_task():
+    """Gets task number (int) from command line input."""
+    
+    print(f'''Tasks:
+          1: Process g09 outputs (opt, freq or sp jobs).
+          2: Write .sh files for g09 input files.
+          3: Process conformational search results (.hcs).
+          4: Change link0 and route lines in existing g09 input files.''')
+    task_num = input("Enter task number:")
+    if not task_num.strip().isdigit():
+        print("Error. Input must be an integer between 1 and 4.")
+        task_num = get_task()
+    else:
+        if int(task_num) > 4 or int(task_num) < 0:
+            print("Error. Input must be an integer between 1 and 4.")
+            task_num = get_task()
+        else:
+            task_num = int(task_num)
+            
+    return task_num
+
+
+#%% functions to get additional arguments according to task
+
+def get_mem():
+    try:
+        mem = int(input("Memory to use (in GB, only write number):"))
+    except ValueError:
+        print("Error. Memory must be an integer.")
+        mem = get_mem()
+    return mem
+
+def get_nproc():
+    try:
+        nproc = int(input("Number of processors to use (nproc):"))
+       
+    except ValueError:
+        print("Error. nproc must be an integer.")
+        nproc = get_nproc()    
+    return nproc
+ 
+def get_charge():
+    try:
+        charge = int(input("Molecule charge:"))
+    except ValueError:
+        print("Error. Charge must be an integer.")
+        charge = get_charge()
+    return charge
+
+def get_mult():
+    try:
+        mult = int(input("Molecule multiplicity:"))
+    except ValueError:
+        print("Error. Multiplicity must be an integer.")
+        mult = get_mult()
+    return mult
+        
+def get_args_1():
+    """Get arguments for task 1 from command line input."""
+    
+    # task 1: Process g09 outputs. Arguments needed for proc_g09out.py
+    
+    path = input("Path to g09 output files:")
+    extension = input("Extension of g09 output files (.log/.out):")
+    sp = input("Only single point calculation? (y/n):")
+    
+    if sp == 'n':
+        get_sp = False
+    else:
+        get_sp = True
+    
+    return path, extension, get_sp
+
+
+def get_args_2():
+    """Get arguments for task 2 from command line input."""
+    
+    # task 2: write .sh files. Arguments needed for write_sh.py
+    
+    path = input("Path to g09 input files:")
+    extension = input("Extension of g09 output files (.com/.gjf):")
+    time = input('Wall clock time for job, format HH:MM:SS:')
+    nfiles = int(input('Number of g09 inputs in each sh (all must have same nproc):'))
+    sname = input("Prefix for .sh filenames:")
+    
+    return path, extension, time, nfiles, sname
+
+
+def get_args_3():
+    """Get arguments for task 3 from command line input."""
+
+    # task 3: Process .HCS file and write g09 input files. 
+    # Arguments needed for hcs_to_g09.py
+    
+    path = input("Path to conformational search (.hcs) files:")
+    extension = input("Extension of g09 output files (.com/.gjf):")
+    
+    mp = input('Default molecular properties? (y/n):')
+    mps = []
+    
+    if mp == 'n':
+        charge = get_charge()
+        mult = get_mult()
+        mps = mps + [charge, mult]
+    
+    gj = input('Default g09 job input? (y/n):')
+    gjs = []
+    
+    if gj == 'n':
+        nproc = get_nproc()
+        mem = get_mem()
+        func = input("Functional:")
+        bset = input("Basis set:")
+        job = input("G09 job keywords and options as would appear in input file:")
+        gjs = gjs + [nproc, mem, func, bset, job]
+    
+    return path, extension, mps, gjs
+    
+
+
+    
+def get_args_4():
+    """Get arguments for task 4 from command line input."""
+    
+    # task 4: rewrite g09 files. Arguments needed for rw_g09in.py
+    path = input("Path to g09 input files:")
+    extension = input("Extension of g09 output files (.com/.gjf):")
+    
+    nproc = get_nproc()
+    mem = get_mem()    
+        
+    route = input("Route line for input (without # symbol):")
+    chk = input("Add chk line to input files? (y/n):")
+    
+    if chk == 'n':
+        chk = False
+    else:
+        chk = True
+    
+    return path, extension, nproc, mem, route, chk
+
+#%% task 1 
+
+def task1():
+    """Get arguments for task 1 and do task."""
+    import proc_g09out
+    
+    path, extension, get_sp = get_args_1()
+    
+    proc_g09out.main(path, extension = extension, get_sp = get_sp)
+
+#%% task 2 
+
+def task2():
+    """Get arguments for task 2 and do task."""
+    import write_sh
+    
+    path, extension, time, nfiles, sname = get_args_2()
+    
+    write_sh.main(path, time, extension = extension, 
+                  n_files = nfiles, sh_name = sname)
+    
+    
+#%% task 3 
+
+def task3():
+    """Get arguments for task 3 and do task."""
+    import hcs_to_g09
+    
+    path, extension, mps, gjs = get_args_3()
+    
+    if mps:
+        charge = mps[0]
+        mult = mps[1]
+    else:
+        charge = 0
+        mult = 1
+    
+    if gjs:
+        nproc = gjs[0]
+        mem = gjs[1]
+        func = gjs[2]
+        bset = gjs[3]
+        job = gjs[4]
+    else:
+        nproc = 4
+        mem = 2
+        func = 'B3LYP'
+        bset = '6-31G*'
+        job = ''
+        
+    
+    hcs_to_g09.main(charge = charge, multiplicity = mult, nproc = nproc, 
+                    mem = mem, func = func, basis = bset, job = job,
+                    extension = extension, pathin = path)
+
+
+#%% task 4 
+
+def task4():
+    """Get arguments for task 4 and do task."""
+    import rw_g09in
+    
+    path, extension, nproc, mem, route, chk = get_args_4()
+    
+    rw_g09in.main(path, extension = extension, route = route, chk = chk, 
+                  mem = mem, nproc = nproc)
+
+
+#%% main
+
+def main():
+    
+    task = get_task()
+    
+    if task == 1:
+        task1()
+    elif task == 2:
+        task2()
+    elif task == 3:
+        task3()
+    elif task == 4:
+        task4()
+
+    
+#%%
+
+if __name__ == '__main__':
+    main()
---
+++ cctools/write_coordSI.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 14 16:33:51 2022
+
+@author: nat
+"""
+
+# write_coordSI.py
+
+"""Write .csv file with information and coordinates from g09 output files 
+in format for Supporting Information."""
+
+#%% modules
+
+import os
+
+from proc_g09out import get_jobs, parse_file, check_term, error_term
+from g09freq import get_freq, get_Nneg
+from g09opt import get_SCF, get_molecule, split_opt, check_opt
+
+#%% get functions from freq
+
+def get_neg(parsed_freq_job):
+    """Get number of negative frequencies from parsed frequency job output.
+    Returns number as integer."""
+    
+    return get_Nneg(get_freq(parsed_freq_job))
+
+def get_free_energy(parsed_freq_job):
+    """Get free energy form parsed frequency job output.
+    Returns energy value (Sum of electronic and thermal Free Energies)."""
+    
+    for line in parsed_freq_job:
+        if 'Sum of electronic and thermal Free' in line:
+            free_energy = float(line.split('=')[1])
+            
+    return free_energy    
+
+
+    
+
+#%% get-out functions, get all info from output
+
+def get_out_freq(parsed_freq_job):
+    """Get information from parsed frequency output to write SI.
+    In: parsed freq output.
+    Out: Energy, Free energy, Number of neg frequencies, 
+    molecule (coordinates in a molecule class object)."""
+    
+    energy = get_SCF(parsed_freq_job)
+    free_energy = get_free_energy(parsed_freq_job)
+    n_neg = get_neg(parsed_freq_job)
+    molecule = get_molecule(parsed_freq_job)
+    
+    
+    return energy, free_energy, n_neg, molecule
+    
+
+    
+def get_out_opt(parsed_opt_out):
+    """Get information from parsed optimization output to write SI.
+    In: parsed opt output.
+    Out: Energy, 
+    molecule (coordinates in a molecule class object)."""
+
+    opt_steps = split_opt(parsed_opt_out)
+    check_opt(parsed_opt_out)    
+    
+    energy = get_SCF(opt_steps[-2]) # final SCF energy
+    molecule = get_molecule(opt_steps[-2]) # final molecule
+    
+    return energy, molecule
+
+def get_out_sp(parsed_sp_out):
+    """Get information from parsed single point output to write SI.
+    In: parsed opt output.
+    Out: Energy, 
+    molecule (coordinates in a molecule class object)."""
+
+    energy = get_SCF(parsed_sp_out) 
+    molecule = get_molecule(parsed_sp_out) 
+    
+    return energy, molecule
+
+#%% list_out
+
+def list_freq_out(energy, free_energy, n_neg, molecule):
+    """Get list output for SI for frequency job. 
+    Out: list of strings, values separated by commas in each string."""
+    
+    freq_out = [f'Energy = {energy}',
+                f'Free Energy = {free_energy}',
+                f'Number of Imaginary Frequencies = {n_neg}',
+                f'Geometry'] + molecule.csvXYZ()
+    
+    return freq_out
+
+def list_scf_out(energy, molecule):
+    """Get list output for SI for opt or SP job. 
+    Out: list of strings, values separated by commas in each string."""    
+    
+    scf_out = [f'Energy = {energy}'] + molecule.csvXYZ()
+    
+    return scf_out
+
+
+#%% job_to_list functions
+
+def freq_to_list(parsed_freq_job):
+    """Get output string list for writing SI from parsed frequency job."""
+    energy, free_energy, n_neg, molecule = get_out_freq(parsed_freq_job)
+   
+    return list_freq_out(energy, free_energy, n_neg, molecule)
+
+def opt_to_list(parsed_opt_job):
+    """Get output string list for writing SI from parsed opt job."""
+    
+    energy, molecule = get_out_opt(parsed_opt_job)
+    
+    return list_scf_out(energy, molecule)
+
+def sp_to_list(parsed_sp_job):
+    """Get output string list for writing SI from parsed SP job."""
+    
+    energy, molecule = get_out_sp(parsed_sp_job)
+    
+    return list_scf_out(energy, molecule)
+
+#%% g09out_to_list
+
+def g09out_to_list(pathin, g09out_name):
+    """Get output string list for writing SI from g09 output file."""
+    
+    jobs, route = get_jobs(os.path.join(pathin, g09out_name))
+    parsed_out = parse_file(os.path.join(pathin, g09out_name), jobs)
+    
+    
+    if 'opt' in jobs:
+        if 'freq' in jobs:
+            # opt and freq job
+            output_list = freq_to_list(parsed_out[1])
+        else:
+            # only opt job
+            output_list = opt_to_list(parsed_out)
+ 
+    elif 'freq' in jobs:
+        # only freq job
+        output_list = freq_to_list(parsed_out)
+    
+    else:
+        # single point
+        output_list = sp_to_list(parsed_out)
+    
+    return output_list
+
+
+
+#%% main function
+
+def main(path, g09_files = None, out_filename = "SI_coords", extension = '.log'):
+    
+    if not g09_files:
+        g09_files = [x for x in os.listdir(path) if x.endswith(extension)]
+    
+    if len(g09_files) == 0:
+        raise ValueError(f'No files of extension {extension} found.')
+    
+    for i, file in enumerate(g09_files):
+        if not check_term(file, path):
+            error_term(file, path)
+            error_file = g09_files.pop(i)
+            print(f'{error_file} did not end in normal termination.')
+    
+    results = {}
+
+    for filename in g09_files:
+        try:
+            results[filename] = g09out_to_list(path, filename)
+            
+        except Exception as e:
+            print(f'Could not process {filename}. Error: {e}')
+    
+    with open(os.path.join(path, out_filename + '.csv'), 'w') as out:
+        for filename, out_list in results.items():
+            out.write(filename + ' \n')
+            out.writelines("%s\n" % l for l in out_list)
+            out.write('\n\n')
+
+
+#%% input parser
+
+
+if __name__ == '__main__':
+    
+    import argparse as ap
+    parser = ap.ArgumentParser(prog = 'write_coordSI', 
+                               description = """Write .csv file with information 
+                               and coordinates from g09 output files 
+                               in format for Supporting Information.""")
+    
+    parser.add_argument('-p', '--path', type = str, default = '.',
+                        help = 'path for directory to work in')
+    parser.add_argument('-i', '--input', type = list, default = None,
+                        help = 'list of g09 output files. defaults to all files in working directory')
+    parser.add_argument('-e', '--ext', type = str, default = '.log',
+                        help = 'extension of g09 output files. if incorrect files wont be found')
+    parser.add_argument('-o', '--out_name', type = str, default = 'SI_coords',
+                        help = 'Name for output .csv file (without extension), defaults to SI_coords')
+ 
+    args = parser.parse_args()
+     
+    main(args.path, g09_files = args.input, out_filename = args.out_name,
+         extension = args.ext)
